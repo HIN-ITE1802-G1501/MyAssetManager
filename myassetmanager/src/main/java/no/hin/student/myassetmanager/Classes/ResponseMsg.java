274f65c491951209df39b5617ed431c0ea9bb7ee
Aleksander V Grunnvoll <vallegrunnvoll@gmail.com>
2015-05-05 19:25:56 +0200

    Added ResponseMsg class

---
+++ myassetmanager/src/main/java/no/hin/student/myassetmanager/Classes/ResponseMsg.java
@@ -0,0 +1,49 @@
+package no.hin.student.myassetmanager.Classes;
+
+import com.google.gson.Gson;
+
+/**
+ * Created by wfa on 07.04.2015.
+ *
+ * Sendes fra server (som JSON) som svar pÃ¥ alle forespÃ¸rsler klienten gjÃ¸r mot serveren.
+ *
+ */
+public class ResponseMsg {
+    private Boolean result = false;     //Indikerer om forespÃ¸rselen gikk bra eller ikke.
+    private String message ="";         //Melding eller feilmelding fra server.
+    private String jsonResponse="";     //Dersom serveren returnerer data, f.eks. en liste med Equipment-objekter, ligger dette her som JSON.
+
+    public Boolean isResult() {
+        return result;
+    }
+
+    public void setResult(Boolean result) {
+        this.result = result;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Boolean getResult() {
+        return result;
+    }
+
+    public String getJsonResponse() {
+        return jsonResponse;
+    }
+
+    public void setJsonResponse(String jsonResponse) {
+        this.jsonResponse = jsonResponse;
+    }
+
+    public String toJSONString() {
+        Gson gson = new Gson();
+        String json = gson.toJson(this);
+        return json;
+    }
+}
